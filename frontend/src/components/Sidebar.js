b0602136f0992c00ce545642bc906e3ac7cd767e
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-03-07 18:13:25 -0500

    I remove the tag link as well bc i do not have enough time to work on it

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -5,7 +5,7 @@ export const Sidebar = (props) => {
   return (
     <nav className="sidebar">
       <div className="profile">
-        <NavLink to={'/'}><img className="profile_pic" alt="" src={props.user.profile_pic ? props.user.profile_pic : "http://icons.iconarchive.com/icons/pelfusion/long-shadow-media/256/Contact-icon.png"} />{props.user.full_name}</NavLink>
+        <NavLink to={'/notes'}><img className="profile_pic" alt="" src={props.user.profile_pic ? props.user.profile_pic : "http://icons.iconarchive.com/icons/pelfusion/long-shadow-media/256/Contact-icon.png"} />{props.user.full_name}</NavLink>
       </div>
       <div className='searhbar'>
         <input type='text' className='searchbar' placeholder='search all notes' />
@@ -13,11 +13,10 @@ export const Sidebar = (props) => {
         <div className='newNote'>
         <NavLink to={'/new'}>New note</NavLink>
         </div>
-        <NavLink to={'/notebooks/:id/notes'}>Shortcuts</NavLink>
+
         <NavLink to={'/notes'}>All notes</NavLink>
         <NavLink to={'/notebooks'}>Notebooks</NavLink>
-        <NavLink to={'/notebooks/2/notes'}>Trash</NavLink>
-        <NavLink to={'/notebooks'}>Tag</NavLink>
+
         <NavLink to={'/logout'} onClick={() => {props.logoutUser()}}>Log out</NavLink>
     </nav>
   )
